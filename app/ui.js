a7a73ed0303ff0b1554cd623115e6fef89abe294
Michael Crane <michael.crane96@gmail.com>
2017-07-06 15:27:08 -0700

    Added submit feature to customization pop up

---
+++ app/ui.js
@@ -0,0 +1,37 @@
+//------------Input Button Stuff----------------------//
+// Reference: http://jsfiddle.net/gregorypratt/dhyzV/ //
+
+    document.getElementById('get_file').onclick = function() {
+       document.getElementById('my_file').click();
+    };
+
+//------------Customize Button Stuff----------------//
+    var modal = document.getElementById('modal');
+    var btn = document.getElementById("cust_file");
+    var exit = document.getElementsByClassName("close")[0];
+
+
+   // modal appears when Custimize Btn is clicked
+   btn.onclick = function() {
+       modal.style.display = "block";
+   }
+
+   // stuff to close modal
+   exit.onclick = function() {
+      modal.style.display = "none";
+   }
+   
+ // run is called when the submit button is clicked
+ // it saves whatever is in each textarea to a variable
+ // TODO: parse the input to work with cartogram
+ function run() {
+  var cust_file_name = document.getElementById("file_name").value;
+  var col_1_input =  document.getElementById("data_col_1").value;
+  var col_2_input =  document.getElementById("data_col_2").value;
+  var col_3_input =  document.getElementById("data_col_3").value;
+  console.log(cust_file_name);
+  console.log(col_1_input);
+  console.log(col_2_input);
+  console.log(col_3_input);
+  
+}
