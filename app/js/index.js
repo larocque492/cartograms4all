4a51d508c35b8ac9fde92d8e89357004898e7d76
root <root@ip-172-31-43-248.us-west-2.compute.internal>
2017-07-06 21:12:18 +0000

    Addressed uncaught reference type error

--- app/js/index.js
+++ app/js/index.js
@@ -22,25 +22,7 @@ $(document).ready(function()
 function init()
 {
   csvFields = getCSVFields(initCartogram);
-          var features = carto.features(topology, geometries),
-            path = d3.geo.path()
-              .projection(proj);
-
-        states = states.data(features)
-          .enter()
-          .append("path")
-            .attr("class", "state")
-            .attr("id", function(d) {
-              return d.properties.NAME;
-            })
-            .attr("fill", "#fafafa")
-            .attr("d", path);
-
-        states.append("title");
-
-        parseHash();
-
-}
+          }
 
 function initCartogram(csvFields)
 {
@@ -142,6 +124,25 @@ d3.json(url, function(topo) {
             .key(function(d) { return d.NAME; })
             .rollup(function(d) { return d[0]; })
             .map(data);
+     var features = carto.features(topology, geometries),
+            path = d3.geo.path()
+              .projection(proj);
+
+        states = states.data(features)
+          .enter()
+          .append("path")
+            .attr("class", "state")
+            .attr("id", function(d) {
+              return d.properties.NAME;
+            })
+            .attr("fill", "#fafafa")
+            .attr("d", path);
+
+        states.append("title");
+
+        parseHash();
+
+
         init();
     });
 });
