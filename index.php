aad393e53b0d89c6734711485c5a1010c9be6d35
Luke Tanner <latanner.ucsc.edu>
2017-07-12 23:49:55 -0700

    myCommit

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 
 <?php 
-if (!isset($_COOKIE['c4a_session_id']))
-{
+
+if (!isset($_COOKIE['c4a_session_id'])) {
     $new_id = session_create_id();
     setCookie("c4a_session_id", $new_id, 8251005, '/');
     exit();
 }
-
-function 
 
 header( 'Location: /app/index.html' ); 
 ?>
