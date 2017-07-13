e428ff383433d86a742149e511badee8b68be47d
jlin53 <jlin53@ucsc.edu>
2017-07-13 16:29:08 +0000

    save local commits

--- app/php/storeCookie.php
+++ app/php/storeCookie.php
@@ -1,16 +1,15 @@
 <?php
-//PHP snippet to add sessionID from user
+//PHP snippet to serve cookie
 //The logic of the cookie will be use as a way to track setting to init
 
 //Setting Current session ID 
 //TODO: Replace session_id by getSessionID from Luke
-$_currentSessionId = session_id();
+$currentSessionId = "10" //session_id();
 
 $cookie_name = "user_profile"
 
 $expires = (86400 * 30);
 
-//setcookie($cookie_name, $_currentSessionId, time() + $_expires, "/"); //30 days
 
 //Generic function to set information in user's cookie
 function set_cookie($cookie_name, $sessionId, $expiration){
