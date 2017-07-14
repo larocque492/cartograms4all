d6d9580a1bea1d21f71126f4bd2b57fc83a903c9
Luke Tanner <latanner@ucsc.edu>
2017-07-13 18:44:16 -0700

    still trying

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 
 <?php 
-echo "before if";
+echo "before if ";
 if (!isset($_COOKIE['c4a_session_id'])) {
-    echo "in if";
+    echo "in if before new_id ";
     $new_id = session_create_id();
+    echo "in if after new_id";
     setCookie("c4a_session_id", $new_id, 8251005, '/');
+    echo "in if after c4a_session_id";
     exit();
 }
 echo "after if";
