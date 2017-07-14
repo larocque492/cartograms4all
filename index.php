de6edf92921d5de4296b9734513ec3bcba267508
Luke Tanner <latanner@ucsc.edu>
2017-07-13 18:41:53 -0700

    still trying

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 
 <?php 
-
+echo "before if";
 if (!isset($_COOKIE['c4a_session_id'])) {
+    echo "in if";
     $new_id = session_create_id();
-    setcookie("c4a_session_id", $new_id);
+    setCookie("c4a_session_id", $new_id, 8251005, '/');
+    exit();
 }
+echo "after if";
 
 header( 'Location: /app/index.html' ); 
 ?>
