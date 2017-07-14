eac7665a3d61f2f8c7d778d61d438100c948b2dc
Luke Tanner <latanner@ucsc.edu>
2017-07-13 18:30:39 -0700

    its 'setcookie()' 'not setCookie'

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 if (!isset($_COOKIE['c4a_session_id'])) {
     $new_id = session_create_id();
-    setCookie("c4a_session_id", $new_id, 8251005, '/');
+    setcookie("c4a_session_id", $new_id, 8251005, '/');
 }
 
 header( 'Location: /app/index.html' ); 
