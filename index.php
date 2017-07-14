541c1574dbadde207644d00af4671781c367de5a
Luke Tanner <latanner@ucsc.edu>
2017-07-13 18:20:23 -0700

    removed cookies setting php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 
 <?php 
 
-if (!isset($_COOKIE['c4a_session_id'])) {
-    $new_id = session_create_id();
-    setCookie("c4a_session_id", $new_id, 8251005, '/');
-    exit();
-}
+//if (!isset($_COOKIE['c4a_session_id'])) {
+//    $new_id = session_create_id();
+//    setCookie("c4a_session_id", $new_id, 8251005, '/');
+//    exit();
+//}
 
 header( 'Location: /app/index.html' ); 
 ?>
