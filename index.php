98cab20f1fb33c6326125b060d444da32ef7825a
Luke Tanner <latanner@ucsc.edu>
2017-07-13 18:47:10 -0700

    session_start()

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <?php 
 echo "before if ";
 if (!isset($_COOKIE['c4a_session_id'])) {
+    session_start();
     echo "in if before new_id ";
     $new_id = session_create_id();
     echo "in if after new_id";
     setCookie("c4a_session_id", $new_id, 8251005, '/');
     echo "in if after c4a_session_id";
-    exit();
 }
 echo "after if";
 
