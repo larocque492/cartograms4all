a999e5f61413242db8fc3a3e1243afdf9074374e
Luke Tanner <latanner.ucsc.edu>
2017-07-14 20:29:38 -0700

    fixed syntax error

--- app/php/import_settings.php
+++ app/php/import_settings.php
@@ -5,6 +5,7 @@ function writeSettingsFile(){
     if(!empty($_POST['data'])){
         $settings_string = $_POST['data'];
         $session_file_name = "settings/";
+        $session_id = "session_id";
         $session_file_name .= ("session_id" .= ".json");
         file_put_contents($session_file_name, $settings_string);
     }
