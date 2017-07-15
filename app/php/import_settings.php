7977415c6911b091a6b1909402476489f0d355fb
Luke Tanner <latanner.ucsc.edu>
2017-07-14 20:26:02 -0700

    temp value for session_id in import_settings.php

--- app/php/import_settings.php
+++ app/php/import_settings.php
@@ -5,7 +5,7 @@ function writeSettingsFile(){
     if(!empty($_POST['data'])){
         $settings_string = $_POST['data'];
         $session_file_name = "settings/";
-        $session_file_name .= ($session_id .= ".json");
+        $session_file_name .= ("session_id" .= ".json");
         file_put_contents($session_file_name, $settings_string);
     }
 }
