ed6d81d0de00a5cb42a115ba75b7619d1897c073
Luke Tanner <latanner@ucsc.edu>
2017-07-16 13:34:45 -0700

    using session_id to retrieve data from server

--- app/php/export_settings.php
+++ app/php/export_settings.php
@@ -1,11 +1,11 @@
 
 <?php
 
-    if(!empty($_GET['name'])){
+    //if(!empty($_GET['name'])){
         $session_file_name = "settings/";
-        $session_id = $_GET['name'];
+        $session_id = 'UleiDetf5pIgPU2a';//$_GET['name'];
         $session_file_name .= ($session_id .= ".json");
         $session_file_string = file_get_contents($session_file_name);
-        echo json_encode($session_file_string);
-    }
+        echo ($session_file_string);
+    //}
 ?>
