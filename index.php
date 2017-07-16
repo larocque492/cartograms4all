13731108115f46e5f19ffe5833e739b75fad47fb
Luke Tanner <latanner@ucsc.edu>
2017-07-16 00:52:44 -0700

    cookie name is 'user_session_id"
    nbd. :)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php 
 
-if (!isset($_COOKIE['c4a_session_id'])) {
+if (!isset($_COOKIE['user_session_id'])) {
     $new_id = session_create_id();
     setCookie("c4a_session_id", $new_id, 8251005, '/');
     exit();
