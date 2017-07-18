edb28bfbe3d8d651d050ba7b6a17fc58393e5cda
jlin53 <jlin53@ucsc.edu>
2017-07-18 03:06:35 +0000

    deploy

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ if (!isset($_COOKIE['user_session_id'])) {
     //$new_id = session_create_id();
     $new_id = "1000";
     setCookie("user_session_id", $new_id, 8251005, '/');
-    exit();
 }
 
 header( 'Location: /app/index.html' ); 
