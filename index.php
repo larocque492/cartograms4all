4eb8e5baf196c4d029f9c47665cf92c2fefb0162
jlin53 <jlin53@ucsc.edu>
2017-07-18 03:17:33 +0000

    deploy

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
 
 //Set sessionID per user on the site before redirecting to main page
-if (!isset($_COOKIE['user_session_id'])) {
-    $new_id = session_create_id();
-    setCookie("user_session_id", $new_id, 8251005, '/');
 }
 
 header( 'Location: /app/index.html' ); 
