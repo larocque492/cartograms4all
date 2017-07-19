c7d22fc72560afcceb58494ed31f9c258bee054d
Michael Crane <mbcrane@ucsc.edu>
2017-07-19 14:17:51 -0700

    update to date with master + cleaned up

--- app/js/ui.js
+++ app/js/ui.js
@@ -1,5 +1,5 @@
 //------------Input Button Stuff----------------------//
-// Reference: http://jsfiddle.net/gregorypratt/dhyzV/ //
+
 document.getElementById('upload_link').onclick = function() {
   document.getElementById('input_csv').click();
 };
@@ -7,6 +7,7 @@ document.getElementById('upload_link').onclick = function() {
 document.getElementById('upload_link_topo').onclick = function() {
   document.getElementById('input_topo').click();
 };
+
 /*
  * Code to run when document is ready
  */
@@ -34,66 +35,6 @@ $(document).ready(function() {
 });
 
 
-//------------Customize Button Stuff----------------//
-var modal = document.getElementById('modal');
-var btn = document.getElementById("cust_file");
-var exit = document.getElementsByClassName("close")[0];
-
-
-// modal appears when Customize Btn is clicked
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-exit.onclick = function() {
-    console.log("cust close");
-    modal.style.display = "none";
-}
-
-
-// run is called when the submit button is clicked
-// it saves whatever is in each textarea to a variable
-// TODO: parse the input to work with cartogram
-function run() {
-  //var cust_file_name = document.getElementById("file_name").value;
-  var col_1_input = document.getElementById("data_col_1").value;
-  var col_2_input = document.getElementById("data_col_2").value;
-  //var col_3_input =  document.getElementById("data_col_3").value;
-  //console.log(cust_file_name);
-  console.log(col_1_input);
-  console.log(col_2_input);
-  //console.log(col_3_input);
-
-}
-
-//------------------Share Cartogram Button-----------//
-var share_modal = document.getElementById('share_modal');
-var share_btn = document.getElementById("share_cartogram");
-var share_exit = document.getElementsByClassName("share_close")[0];
-
-share_btn.onclick = function() {
-  share_modal.style.display = "block";
-}
-
-share_exit.onclick = function() {
-    console.log("close share");
-    share_modal.style.display = "none";
-}
-
-
-//------------------Publish Cartogram Button-----------//
-var publish_modal = document.getElementById('publish_modal');
-var publish_btn = document.getElementById("download_cartogram");
-var publish_exit = document.getElementsByClassName("publish_close")[0];
-
-publish_btn.onclick = function() {
-  publish_modal.style.display = "block";
-}
-
-publish_exit.onclick = function() {
-    console.log("close publish");
-    publish_modal.style.display = "none";
-}
 
 
 function download_png(){
@@ -107,7 +48,7 @@ function download_png(){
 $('#download_svg').click(function(){
         var a      = document.createElement('a');
 		a.href     = 'data:image/svg+xml;utf8,' + unescape($('#map')[0].outerHTML);
-		a.download = 'cartogram.svg';
+		a.download = 'svg_info.svg';
 		a.target   = '_blank';
 		document.body.appendChild(a); a.click(); document.body.removeChild(a);
 	});
