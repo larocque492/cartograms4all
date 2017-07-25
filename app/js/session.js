552791f43c76a54750943539b95113456d4c6cd9
Luke Tanner <latanner.ucsc.edu>
2017-07-25 12:08:46 -0700

    Pre/Post comments and removed save check that crippled functionality

--- app/js/session.js
+++ app/js/session.js
@@ -1,10 +1,14 @@
-// sets flags when writing the user's current CSV to the server
+// Pre: n/a
+// Post: saveFlag == false
+// sets flags when writing the user's current CSV to the server && <userSessionID>.csv exists on the server
 function saveSession() {
     saveFlag = true;
     init();
     saveFlag = false;
 }
 
+// Pre: <userSessionID>.csv exists on the server
+// Post: serverDownloadFlag == true, userUploadFlag == false && && userData = nameOfLoadFile
 // sets flags and file name when loading current user's CSV from server
 function loadMySession() {
     if (haveSavedFlag) {
@@ -17,12 +21,12 @@ function loadMySession() {
     }
 }
 
+// Pre: user-input session id == 16 chars and is valid (i.e. has an associated .csv file on our server)
+// Post: serverDownloadFlag = true && serUploadFlag == false && userData = nameOfLoadFile
 // sets flags and file name when loading other user's CSV from server
 function loadOtherSession() {
     if (nameOfLoadFile.length != 27) {
         alert("Error: invalid session ID. Please enter a valid session ID.");
-    } else if (nameOfLoadFile.substring(7, 23) == userSessionID && !haveSavedFlag) {
-        alert("Error: you entered your own session ID but have no info saved. Please save your session info.");
     } else {
         serverDownloadFlag = true;
         userUploadFlag = false;
@@ -30,11 +34,15 @@ function loadOtherSession() {
     }
 }
 
+// Pre: userSessionID == readCookie('userSessionCookie')
+// Post: sharing form contains userSessionID in readonly form
 // loads the session ID into sharing form
 function shareSessionID(element) {
     if (userSessionID != null) element.value = userSessionID;
 }
 
+// Pre: user-input session id has 16 char length and is valid (i.e. has an associated .csv file on our server)
+// Post: userData = nameOfLoadFile
 // gets a session ID from user and uses it to load the corresponding user's CSV
 document.getElementById('paste_session_id').onkeydown = function(event) {
     var e = event || windows.event;
@@ -44,6 +52,9 @@ document.getElementById('paste_session_id').onkeydown = function(event) {
     }
 }
 
+// Pre: none
+// Post: return 16 char alphanumeric string
+// returns a random 16 character alphanumeric string to be used as a session ID
 function generateSessionID(length) {
     var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var result = '';
@@ -54,6 +65,8 @@ function generateSessionID(length) {
     return result;
 }
 
+// Pre: session_id is valid && string_to_save != null
+// Post: string_to_save is stored as the contents of <session_id>.json on the server
 // writes string_to_save to app/php/settings/<session_id>.json
 function writeToServer(session_id, string_to_save) {
     var data = new FormData();
@@ -64,6 +77,8 @@ function writeToServer(session_id, string_to_save) {
     XHR.send(data);
 }
 
+// Pre: <session_id>.json exists on server
+// Post: returns a string representation of the contents of <session_id>.json
 // returns contents from app/php/settings/<session_id>.json as a string
 function readFromServer(session_id) {
     var return_string;
@@ -81,9 +96,11 @@ function readFromServer(session_id) {
     return return_string;
 }
 
-//Import cookie information through an API that reads the content of sessionId.json
-//After importing, it will try to pull out the critical information like file/settings
-//It then sets it for the user
+// Pre: readFromServer(sessionID) returns a valid .json string
+// Post: current user now uses settings loaded from server
+// Import cookie information through an API that reads the content of sessionId.json
+// After importing, it will try to pull out the critical information like file/settings
+// It then sets it for the user
 function importUserSettings(sessionId) {
     //call API
     var jsonString = readFromServer(sessionId); // jsonString is read in from the correct file in php/settings folder
@@ -93,7 +110,9 @@ function importUserSettings(sessionId) {
     init(); //refresh the view
 }
 
-//Export cookie information and call API to write file as sessionId.json
+// Pre: userCookieJson is initialized
+// Post: <session_id>.json stores user settings on the server
+// Export cookie information and call API to write file as sessionId.json
 function exportUserSettings() {
     createCookie("fileName", userData, 30, "/");
     var userCookieJson = exportCookie(); // a string representation of the JSON
@@ -103,8 +122,9 @@ function exportUserSettings() {
     writeToServer(session_id, userCookieJson);
 }
 
-//Save CSV to uploader/upload path via an ajax call
-//The saved CSV can be use for other user as it is public
+// Pre: userCSV is a DOM object containing a valid userCSV
+// Post: <userSessionID>.csv is stored on the server
+// Save a .csv to the uploader/upload on the server via an ajax call.
 function saveByFile(userCSV) {
     var data = new FormData();
     data.append("input_csv", userCSV);
@@ -131,8 +151,10 @@ function saveByFile(userCSV) {
     haveSavedFlag = true;
 }
 
-//Save CSV to uploader/upload path via an ajax call
-//The saved CSV can be use for other user as it is public
+// Pre: userSessionID is valid && nameOfLoadFile is a valid path to a .csv on the server
+// Post: a new <userSessionID>.csv is stored on the server
+// Save CSV to uploader/upload path given the name of the file via an ajax call
+// The saved CSV can be use for other user as it is public
 function saveByName() {
     var data = new FormData();
     data.append("userID", userSessionID);
